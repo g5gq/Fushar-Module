1156013c04491ee6cc9de22526f32ce2a3164a00
g5gq <nizarzdz331@gmail.com>
2025-07-04 00:44:46 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,48 +1,41 @@
-export async function searchResults(query) {
-    const tmdbKey = "68e094699525b18a70bab2f86b1fa706";
-    const url = `https://api.themoviedb.org/3/search/movie?api_key=${tmdbKey}&query=${encodeURIComponent(query)}`;
+/* jshint esversion: 11 */
+/* global soraFetch */
 
-    const res = await fetch(url);
-    const json = await res.json();
-    const results = json.results.map(movie => ({
-        id: movie.id.toString(),
-        title: movie.title,
-        image: "https://image.tmdb.org/t/p/w500" + movie.poster_path,
-        description: movie.overview,
-    }));
+async function searchResults(keyword) {
+  const url = `https://api.themoviedb.org/3/search/movie?api_key=68e094699525b18a70bab2f86b1fa706&query=${encodeURIComponent(keyword)}`;
+  const r = await soraFetch(url);
+  if (!r || r.status !== 200) return JSON.stringify([]);
 
-    return JSON.stringify(results);
+  const data = await r.json();
+  return JSON.stringify(data.results.map(movie => ({
+    title: movie.title,
+    href: movie.id.toString(),
+    image: movie.poster_path ? `https://image.tmdb.org/t/p/w500${movie.poster_path}` : "",
+  })));
 }
 
-export async function extractDetails(id) {
-    const tmdbKey = "68e094699525b18a70bab2f86b1fa706";
-    const url = `https://api.themoviedb.org/3/movie/${id}?api_key=${tmdbKey}`;
+async function extractDetails(movieId) {
+  const url = `https://api.themoviedb.org/3/movie/${movieId}?api_key=68e094699525b18a70bab2f86b1fa706`;
+  const r = await soraFetch(url);
+  if (!r || r.status !== 200) return JSON.stringify({});
 
-    const res = await fetch(url);
-    const movie = await res.json();
-
-    return JSON.stringify({
-        title: movie.title,
-        image: "https://image.tmdb.org/t/p/w500" + movie.poster_path,
-        description: movie.overview,
-        genres: movie.genres.map(g => g.name),
-    });
+  const m = await r.json();
+  return JSON.stringify({
+    description: m.overview || "",
+    aliases: `Released: ${m.release_date || "Unknown"}`,
+    airdate: m.release_date || "",
+    extra: `Genres: ${m.genres.map(g => g.name).join(", ")}`
+  });
 }
 
-export async function extractEpisodes(id) {
-    return JSON.stringify([
-        {
-            id: id,
-            title: "Watch Now",
-        }
-    ]);
+async function extractEpisodes(movieId) {
+  return JSON.stringify([{ href: movieId, number: 1 }]);
 }
 
-export async function extractStreamUrl(id) {
-    return JSON.stringify({
-        stream: `https://vidsrc.to/embed/movie/${id}`,
-        headers: {
-            Referer: "https://vidsrc.to/",
-        }
-    });
+async function extractStreamUrl(movieId) {
+  const streamUrl = `https://vidsrc.to/embed/movie/${movieId}`;
+  return JSON.stringify({
+    streams: [{ title: "Watch", streamUrl }],
+    subtitles: []
+  });
 }
